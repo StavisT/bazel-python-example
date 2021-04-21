8a57913ec888d5d63aa4b5caab6724727414b6ba
Simon Stavland <simon.stavland@cognite.com>
2021-04-21 14:58:11 +0200

    create simple flask service

---
+++ src/flask_service/main.py
@@ -0,0 +1,15 @@
+import logging
+import re
+import sys
+
+from gunicorn.app.wsgiapp import run
+from gunicorn.http import message
+
+
+if __name__ == "__main__":
+    message.MAX_REQUEST_LINE = 2048
+    sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
+    
+    logger.info("there is no function to execute in this example")
+
+    sys.exit(run())  # to help k8s
